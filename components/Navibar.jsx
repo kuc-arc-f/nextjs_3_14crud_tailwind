4e67debea72e93eae41c27cc554760dcb243f61a
中島浩二 <knaka0209@gmail.com>
2021-05-07 15:46:23 +0900

    up,0507a5

--- components/Navibar.jsx
+++ components/Navibar.jsx
@@ -26,60 +26,66 @@ export default class Navibar extends React.Component {
   }    
   render(){
     return (
-    <div className="bg-white ">
-      <div className="container mx-auto">
-        <ul className="flex flex-wrap py-2 px-4">
-          <li className="w-full py-1 pr-6 sm:w-auto sm:py-0">
-            <Link href="/">
-              <a className="text-blue-500 hover:text-blue-800 text-3xl font-bold">
-                CRUD Sample
+    <div>
+      <div className="navi_main_wrap navi_bg_color">
+        <div className="container mx-auto">
+          <ul className="flex flex-wrap py-2 px-4">
+            <li className="w-full py-1 pr-6 sm:w-auto sm:py-0">
+              <Link href="/">
+                <a className="navi_text_color text-3xl font-bold">
+                  CRUD Sample
+                </a>
+              </Link>
+            </li>
+            <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
+              <Link href="/">
+                <a className="navi_text_color">Home</a>
+              </Link>
+            </li>
+            <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
+              <Link href="/tasks">
+                <a className="navi_text_color">Task</a>
+              </Link>
+            </li>          
+            <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
+              <Link href="/books">
+                <a className="navi_text_color">Books</a>
+              </Link>
+            </li>          
+            <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
+              <a className="navi_text_color" href="#"
+              onClick={this.clickHandler.bind(this , 1, 1)} >
+              Master <span className="text-xs">▼</span>
               </a>
-            </Link>
-          </li>
-          <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
-            <Link href="/">
-              <a className="text-blue-500 hover:text-blue-800">Home</a>
-            </Link>
-          </li>
-          <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
-            <Link href="/tasks">
-              <a className="text-blue-500 hover:text-blue-800">Task</a>
-            </Link>
-          </li>          
-          <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
-            <Link href="/books">
-              <a className="text-blue-500 hover:text-blue-800">Books</a>
-            </Link>
-          </li>          
-          <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
-            <a className="text-blue-500 hover:text-blue-800" href="#"
-            onClick={this.clickHandler.bind(this , 1, 1)} >
-            Master <span className="text-xs">▼</span>
-            </a>
-          </li>        
-          <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
-            <Link href="#">
-              <a id="openModal"
-              className="text-blue-500 hover:text-blue-800">About</a>
-            </Link>
-          </li>                  
-        </ul>
-        {this.state.subDisplay ==0 ? ""
-        : (<NavibarSub subDisplay={this.state.subDisplay}
-           subData={this.state.subData} />)
-        }
-        <Modal name={this.state.modal_name}
-          title="About" 
-          body={`${process.env.MY_SITE_NAME} ,
-          version : ${process.env.MY_SYS_VERSION}`}>
-        </Modal>        
-      </div>
+            </li>        
+            <li className="w-full pr-4 pb-1 pt-1 sm:pt-2 sm:w-auto sm:py-0">
+              <Link href="#">
+                <a id="openModal"
+                className="navi_text_color">About</a>
+              </Link>
+            </li>                  
+          </ul>
+          <Modal name={this.state.modal_name}
+            title="About" 
+            body={`${process.env.MY_SITE_NAME} ,
+            version : ${process.env.MY_SYS_VERSION}`}>
+          </Modal>        
+        </div>
+      </div>      
       <style>{`
       .is-show {
         visibility: visible;
         opacity : 1;
       }
-      `}</style>       
+      `}</style> 
+      <div className="navi_submain_wrap sub_navi_bg_color">
+        <div className="container mx-auto">
+        {this.state.subDisplay ==0 ? ""
+        : (<NavibarSub subDisplay={this.state.subDisplay}
+          subData={this.state.subData} />)
+        }
+        </div>
+      </div>      
     </div>
     )
   }
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import '../styles/components/buttons.css'
 import '../styles/components/forms.css'
 import '../styles/components/bgcolor.css'
 //import '../styles/components/bgcolor-geen.css'
+import '../styles/components/navi_color_dark.css'
+//import '../styles/components/navi_color.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
